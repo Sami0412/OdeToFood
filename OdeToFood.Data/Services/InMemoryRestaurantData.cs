48d9abd442768b29957be0cdebeb542c86546754
Sami0412 <samiw0412@gmail.com>
2022-02-16 15:29:07 +0000

    Using Controllers

--- OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -22,5 +22,10 @@ namespace OdeToFood.Data.Services
         {
             return restaurants.OrderBy(r => r.Name);
         }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
     }
 }
